9905f52cddd68121288b9ac8336b73250b3162b4
ytyaru <ytyaru@outlook.jp>
2021-06-05 08:56:22 +0900

    comment

--- src/is_whitish.py
+++ src/is_whitish.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf8
-# 2色のコントラストを返す。4.5以上なら見やすい。
-# https://ja.wikipedia.org/wiki/Help:%E9%85%8D%E8%89%B2%E3%81%AE%E3%82%B3%E3%83%B3%E3%83%88%E3%83%A9%E3%82%B9%E3%83%88%E6%AF%94
+# 引数の色が白系なら0を返し、黒系なら1を返す。
 if __name__ == "__main__":
     import argparse, contrast
     parser = argparse.ArgumentParser(description='引数の色が白系なら0を返し、黒系なら1を返す。色は第一引数にてRGB値(255,255,255)形式で受け付ける。')
