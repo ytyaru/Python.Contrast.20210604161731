784168d17484749b4115ea27c43fd8038ecad157
ytyaru <ytyaru@outlook.jp>
2021-06-05 08:55:12 +0900

    argparse

--- src/contrast.py
+++ src/contrast.py
@@ -2,18 +2,11 @@
 # coding: utf8
 # 2色のコントラストを返す。4.5以上なら見やすい。
 # https://ja.wikipedia.org/wiki/Help:%E9%85%8D%E8%89%B2%E3%81%AE%E3%82%B3%E3%83%B3%E3%83%88%E3%83%A9%E3%82%B9%E3%83%88%E6%AF%94
-def contrast(*args):
-    if 1 < len(args):
-        if args[1] == 0: return '0;0;0'
-        if args[1] == 1: return '255;255;255'
-        lum1 = _contrast(args[0])
-        lum2 = _contrast(args[1])
-        def test(high, low): return (high + 0.05) / (low + 0.05)
-        return test(lum1, lum2) if lum1 > lum2 else test(lum2, lum1)
-    else: return rgb2luminance(**args[0].split(','));
-
-def _contrast(color):
-    return rgb2luminance(color.split(','));
+def contrast(color1, color2):
+    lum1 = rgb2luminance(color1.split(','))
+    lum2 = rgb2luminance(color2.split(','))
+    def test(high, low): return (high + 0.05) / (low + 0.05)
+    return test(lum1, lum2) if lum1 > lum2 else test(lum2, lum1)
 
 def rgb2luminance(rgb):
     r = luminance_x(int(rgb[0]))
@@ -26,6 +19,10 @@ def luminance_x(x):
     return (x/12.92) if x <= 0.03928 else pow((x+0.055)/1.055, 2.4)
 
 if __name__ == "__main__":
-    import sys
-    print('%f' % contrast(sys.argv[1], sys.argv[2]))
+    import argparse
+    parser = argparse.ArgumentParser(description='2色のコントラスト比を返す。')
+    parser.add_argument('color1', help='色1。R,G,Bのようにカンマ区切りで渡す。0<=R,G,B<=255')
+    parser.add_argument('color2', help='色2。R,G,Bのようにカンマ区切りで渡す。0<=R,G,B<=255')
+    args = parser.parse_args()
+    print('%f' % contrast(args.color1, args.color2))
 
